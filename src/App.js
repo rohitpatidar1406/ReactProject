c4f72bcdca2165803b35bab046c5be3a0248b1f0
Rohit Patidar <rohit.patidar1406@gmail.com>
2023-02-01 03:20:00 +0530

    committed by Rohit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./components/ExpenseItem/ExpenseItem"
+import Expenses from "./components/ExpenseItem/Expenses"
 
 
 function App() {
@@ -28,26 +28,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem title={expenses[0].title}
-                   amount={expenses[0].amount}
-                   date={expenses[0].date}
-                   id={expenses[0].id}
-      />
-      <ExpenseItem title={expenses[1].title}
-                   amount={expenses[1].amount}
-                   date={expenses[1].date}
-                   id={expenses[1].id}
-      />
-      <ExpenseItem title={expenses[2].title}
-                   amount={expenses[2].amount}
-                   date={expenses[2].date}
-                   id={expenses[2].id}
-      />
-      <ExpenseItem title={expenses[3].title}
-                   amount={expenses[3].amount}
-                   date={expenses[3].date}
-                   id={expenses[3].id}
-      />
+      <Expenses items={expenses}/>
       
     </div>
   );
---
+++ src/components/ExpenseItem/Expenses.js
@@ -0,0 +1,30 @@
+import ExpenseItem from './ExpenseItem';
+import './Expenses.css';
+function Expenses(props)
+{
+    return (<div>
+        <ExpenseItem title={props.items[0].title}
+                   amount={props.items[0].amount}
+                   date={props.items[0].date}
+                   id={props.items[0].id}
+        />
+      <ExpenseItem title={props.items[1].title}
+                   amount={props.items[1].amount}
+                   date={props.items[1].date}
+                   id={props.items[1].id}
+        />
+      <ExpenseItem title={props.items[2].title}
+                   amount={props.items[2].amount}
+                   date={props.items[2].date}
+                   id={props.items[2].id}
+        />
+      <ExpenseItem title={props.items[3].title}
+                   amount={props.items[3].amount}
+                   date={props.items[3].date}
+                   id={props.items[3].id}
+        />
+    </div>)
+
+}
+
+export default Expenses;
